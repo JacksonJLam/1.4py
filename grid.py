4e347382c9d89215c93dff22e67f18d985db9200
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-12-12 12:43:23 -0500

    Update grid.py

--- grid.py
+++ grid.py
@@ -1,15 +1,17 @@
-#graph.py
+#grid.py
 #Jackson Lambert 12-12-18
 #Makes a standard 4 quadrant grid
 from graphics import *
 
 def main() :
+    #Draw axis and set graphing window
     win = GraphWin("Grid", 505,505)
     yaxis = Line(Point(250, 500), Point(250, 0))
     xaxis = Line(Point(500, 250), Point(0,250))
     xaxis.draw(win)
     yaxis.draw(win)
     i = 0
+    #Loop ticks
     for i in range(11) :
         tickx = Line(Point(i*50, 260), Point(i*50, 240))
         tickx.draw(win)
