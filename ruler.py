e78ca29de54bb76e3de7d29ce6b91e35a9d36b97
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-12-13 08:19:09 -0500

    Add files via upload

---
+++ ruler.py
@@ -0,0 +1,16 @@
+#ruler.py
+#Jackson Lambert 12-13-18
+#creates a ruler with cm and in
+
+from graphics import *
+
+def main() :
+    win = GraphWin("Ruler", 1200, 500)
+    Rectangle(Point(10,300), Point(1160,200)).draw(win)
+    for i in range(13) :
+        Line(Point(10 + 95.83 * i, 200), Point(10 + 95.83 * i, 225)).draw(win)
+        Text(Point(10+ 95.83 * i, 232), 0 + 1 * i).draw(win)
+    for i in range(49) :
+        Line(Point(10 + 23.95 * i, 200), Point(10 + 23.95 * i, 215)).draw(win)
+  
+main()
