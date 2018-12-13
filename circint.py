d4e9160d882d156bac86b3fa2318cf34c138e065
JacksonJLam <45007948+JacksonJLam@users.noreply.github.com>
2018-12-13 09:25:29 -0500

    Update circint.py

--- circint.py
+++ circint.py
@@ -8,7 +8,7 @@ def main() :
     win = GraphWin("Circle Window", 400, 400)
     
     inputs = GraphWin("Input Window", 400,400)
-   
+   #printing text
     Text(Point(200,75),"Y Intercept").draw(inputs)
     Text(Point(200,125),"Circle Radius").draw(inputs)
     input1 = Entry(Point(200,150), 20)
@@ -19,17 +19,26 @@ def main() :
     calc.draw(inputs)
     inputs.getMouse()
     inputs.close()
+    #Getting values from entry box
     radius= float(input1.getText())
     intercept= float(input2.getText())
     Circle(Point(0,0), radius).draw(win)
+    #Drawing Y Int line
     Line(Point(-10, intercept), Point(10, intercept)).draw(win).setOutline("blue")
+    #Setting the window to have these graph values
     win.setCoords(-10, -10, 10, 10)
+    #Equation to find x int
     xint = math.sqrt((radius**2) - (intercept**2))
+    #Drawing intercept points
     Circle(Point(xint, intercept),.25).draw(win).setFill("red")
     Circle(Point(-1 * xint, intercept),.25).draw(win).setFill("red")
     xint = str(xint)
     printxint = "-/+" + xint
     Text(Point(3,9), "The X intercepts of the line & circles are").draw(win)
     Text(Point(5,8), printxint).draw(win)
+    Text(Point(0,-2), "Click twice to close").draw(win)
+    win.getMouse()
+    win.getMouse()
+    win.close()
 main()
     
